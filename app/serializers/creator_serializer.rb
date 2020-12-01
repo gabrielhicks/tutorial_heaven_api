2afcad2adf78d69ddc94f0335b052d9da5571b3a
gabrielhicks <gabrielhicks@mail.com>
2020-12-01 15:44:55 -0500

    modal

--- app/serializers/creator_serializer.rb
+++ app/serializers/creator_serializer.rb
@@ -1,5 +1,5 @@
 class CreatorSerializer < ActiveModel::Serializer
-    attributes :id, :title, :content, :image_url, :status, :comments, :user, :github, :difficulty
+    attributes :id, :title, :content, :image_url, :status, :comments, :user, :github, :difficulty, :created_at
     
     def comments
         ActiveModelSerializers::SerializableResource.new(object.comments, each_serializer: CommentSerializer)
